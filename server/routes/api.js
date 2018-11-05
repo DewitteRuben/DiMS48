2ec90b6110230bc52ae8787680d434ebe001b9d7
Unknown <jonas.de.ridder@student.howest.be>
2018-11-05 12:47:23 +0100

    BACKEND: start thingies
    add api router

---
+++ server/routes/api.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/listTests', function(req,res){
+  res.json(''); // TODO: Send actual data
+})
+
+module.exports = router;
