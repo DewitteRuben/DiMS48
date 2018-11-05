37b8fef8c668a5610368884352258a979a1d186d
Unknown <jonas.de.ridder@student.howest.be>
2018-11-05 13:35:26 +0100

    BACKEND add endpoints
    Added endpoints (they dont do anything yet)

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,4 +5,29 @@ router.get('/listTests', function(req,res){
   res.json(''); // TODO: Send actual data
 })
 
+router.get('/dims48Begin', function(req,res){
+  res.json(); // TODO: Send all related instructions + images
+})
+
+router.get('/dims48Part2', function(req,res){
+  res.json(); // TODO: Send all related instructions + images
+})
+
+router.get('/results', function(req,res){
+  res.json(); // TODO: send all results from both parts
+})
+
+router.get('/results/:id', function(req,res){
+  let id = req.params.id;
+  res.json(); // TODO: send results for test with id X
+})
+
+router.post('/resultsPart1', function(req,res){
+  res.json(); // TODO: Submit results to db and return testId
+})
+
+router.post('/resultsPart2', function'req,res){
+  // TODO: submit results to db
+}
+
 module.exports = router;
