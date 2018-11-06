84d8a109b1d5a1ab11eac3830e6e55693eab754e
Robin Staelens <robin.staelens@student.howest.be>
2018-11-06 10:10:43 +0100

    [BACKEND] Added database models

--- server/models/DiMS48Models.js
+++ server/models/DiMS48Models.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const defaultModels = require('./defaultModels.js');
+
 const ResultsPart1Schema = new Schema({
   id: Number,
-  answersPhase1: [Answer],
-  answersPhase2: [Answer]
+  answersPhase1: [defaultModels.Answer],
+  answersPhase2: [defaultModels.Answer]
 });
 
 const ResultsPart2Schema = new Schema({
@@ -9,7 +14,33 @@ const ResultsPart2Schema = new Schema({
   answers: [Answer]
 });
 
+
+const ImagesSchema = new Schema({
+    images: [defaultModels.Image]
+});
+
+const Result = new Schema({
+    timestamp: {type: Date, default: Date.now},
+    clientInfo: defaultModels.ClientInfo,
+    phase1: ResultsPart1Schema,
+    phase2: ResultsPart1Schema,
+});
+
+const Results = new Schema({
+   results: [Result]
+});
+
+const InstructionSchema = new Schema({
+    clientInstruction: defaultModels.Instruction,
+    leaderInstruction: defaultModels.Instruction
+});
+
+const InstructionsSchema = new Schema({
+    instructions: [ InstructionSchema ]
+});
+
 module.exports = {
-  ResultsPart1: mongoose.model('ResultsPart1', ResultsPart1Schema),
-  ResultsPart2: mongoose.model('ResultsPart2', ResultsPart2Schema)
-}
+  Images: mongoose.model('Images', ImagesSchema),
+  Results: mongoose.model('Results', Results),
+  Instructions: mongoose.model('Instructions', InstructionsSchema),
+};
