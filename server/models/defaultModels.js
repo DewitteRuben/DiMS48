d2ea60f2b191e4323ff144d624a7673810b19c7b
Unknown <jonas.de.ridder@student.howest.be>
2018-11-06 09:45:46 +0100

    BACKEND: reorganise models

--- server/models/defaultModels.js
+++ server/models/defaultModels.js
@@ -25,22 +25,9 @@ const AnswerSchema = new Schema({
   correctAnswer: String
 });
 
-const ResultsPart1Schema = new Schema({
-  id: Number,
-  answersPhase1: [Answer],
-  answersPhase2: [Answer]
-});
-
-const ResultsPart2Schema = new Schema({
-  id: Number,
-  answers: [Answer]
-});
-
 module.exports = {
   Instruction: mongoose.model('Instruction', InstructionsSchema),
   Image: mongoose.model('Image', ImagesSchema),
   ClientInfo: mongoose.model('ClientInfo', ClientInfoSchema),
-  Answer: mongoose.model('Answer', AnswerSchema),
-  ResultsPart1: mongoose.model('ResultsPart1', ResultsPart1Schema),
-  ResultsPart2: mongoose.model('ResultsPart2', ResultsPart2Schema)
+  Answer: mongoose.model('Answer', AnswerSchema)
 };
