ac19dd65470855aaa20354e4e8330e06e9e76449
Robin Staelens <robin.staelens@student.howest.be>
2018-11-06 10:20:19 +0100

    [BACKEND] Added mongoose connection

--- server/app.js
+++ server/app.js
@@ -2,15 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
+
+const mongoConfig = require('./config/config');
+mongoose.connect(`${mongoConfig.URI}:${mongoConfig.port}/${mongoConfig.databaseName}`, {useNewUrlParser: true});
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
