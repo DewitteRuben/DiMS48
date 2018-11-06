65d2d2ba2d2ff2c03756f0a0c973cad7132c2d0d
kinablelieselot <lieselot.kinable@student.howest.be>
2018-11-06 13:53:26 +0100

    FRONTEND questions are dynamic

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import dims48instructions from "./modules/tests/dims48/instructions";
+import dims48questions from "./modules/tests/dims48/questions";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    dimsInstructions: dims48instructions
+    dimsInstructions: dims48instructions,
+    dimsQuestions: dims48questions
   }
 });
--- client/src/tests/dims48/components/Dims48Test.vue
+++ client/src/tests/dims48/components/Dims48Test.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
-    <InstructionsForm
+    <!-- <InstructionsForm
       :instructions="currentInstruction.message"
       :personTitle="currentInstruction.title"
       :buttonText="this.$store.state.dimsInstructions.buttonText"
-    />
+    /> -->
+<SingleQuestion
+image="https://www.howtotest.be/sites/all/themes/howtotest/extensions/dms-48/images/single/afbeelding-1.jpg" 
+:options="this.$store.state.dimsQuestions.options.phase1Options">
+</SingleQuestion>
+
   </div>
 </template>
 
 <script>
 import InstructionsForm from "./InstructionsForm.vue";
+import SingleQuestion from "@/tests/dims48/components/SingleQuestion.vue";
 
 export default {
   components: {
-    InstructionsForm
+    InstructionsForm,
+    SingleQuestion
   },
   computed: {
     currentInstruction: function() {
