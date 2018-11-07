99d2c0049d4eeb7a890f0a52c1276afc39141d34
Robin Staelens <robin.staelens@student.howest.be>
2018-11-07 11:07:14 +0100

    [BACKEND] Added imports for seeders

--- server/test/seeders/test.seeder.spec.js
+++ server/test/seeders/test.seeder.spec.js
@@ -4,3 +4,5 @@ const expect = require('chai').expect;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
+
+const seederToTest = require('../../seeders/testsSeeder');
