d958a1251fb4599148c50a4603affdaabd0b9095
kinablelieselot <lieselot.kinable@student.howest.be>
2018-11-07 11:31:18 +0100

    FRONTEND compoments for the test results of a client

---
+++ client/src/tests/dims48/components/ResultTablesClient.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+   <v-data-table
+      :headers="testData.headers_fase1"
+      :items="testData.data_fase1"
+      hide-actions
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-md-left">{{ props.item.percentage }}</td>
+      </template>
+    </v-data-table>
+
+     <v-data-table
+      :headers="testData.headers_fase2"
+      :items="testData.data_fase2"
+      hide-actions
+      class="elevation-1"
+    >
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td class="text-md-left">{{ props.item.percentage }}</td>
+      </template>
+    </v-data-table>
+</div>
+</template>
+
+<script>
+
+import testData from "@/data/testResultsMockData.json";
+
+export default {
+  data() {
+    return { testData };
+  },
+}
+</script>
