85040c160febba06e0e507aa31ec1693efb05a41
Unknown <jonas.de.ridder@student.howest.be>
2018-11-15 14:34:26 +0100

    BACKEND more logging

--- server/seeders/seeder.js
+++ server/seeders/seeder.js
@@ -53,6 +53,8 @@ function checkTests(){
   console.log('Checking tests');
   const queryTests = Test.find();
   queryTests.exec((err, data)=>{
+    if(err)console.log(err);
+    console.log(data);
     if (data.length <= 0){
       console.log('Tests need seeding');
       getTests().forEach(test=> test.save());
