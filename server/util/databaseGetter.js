880ee09865fe35178dd31a4e1533901d45eceb22
Robin Staelens <robin.staelens@student.howest.be>
2018-11-22 09:51:49 +0100

    [BACKEND] Fixed error connecting to database

--- server/util/databaseGetter.js
+++ server/util/databaseGetter.js
@@ -5,19 +5,23 @@ let TestsConfig;
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-// if (isProduction) {
+if (isProduction) {
     DiMSConfig = require('../config/DiMS48/mongo.production.config');
     TestsConfig = require('../config/Tests/mongo.production.config');
-// }else {
-//     DiMSConfig = require('../config/DiMS48/mongo.development.config');
-    // TestsConfig = require('../config/Tests/mongo.development.config');
-// }
+} else {
+    DiMSConfig = require('../config/DiMS48/mongo.development.config');
+    TestsConfig = require('../config/Tests/mongo.development.config');
+}
 
 const DiMS48ConnectionString = `${DiMSConfig.prefix}${DiMSConfig.user}${(DiMSConfig.user !== '' && DiMSConfig.password !== '') ? ':' : ""}${DiMSConfig.password}@${DiMSConfig.URI}:${DiMSConfig.port}/${DiMSConfig.databaseName}`;
 const testConnectionString = `${TestsConfig.prefix}${TestsConfig.user}${(TestsConfig.user !== '' && TestsConfig.password !== '') ? ':' : ""}${TestsConfig.password}@${TestsConfig.URI}:${TestsConfig.port}/${TestsConfig.databaseName}`;
 
-const DiMS48Database = mongoose.createConnection(DiMS48ConnectionString, {useNewUrlParser: true});
-const TestDatabase = mongoose.createConnection(testConnectionString, {useNewUrlParser: true});
+const DiMS48Database = mongoose.createConnection(DiMS48ConnectionString, {
+    useNewUrlParser: true
+});
+const TestDatabase = mongoose.createConnection(testConnectionString, {
+    useNewUrlParser: true
+});
 
 module.exports = {
     DiMS48Database,
