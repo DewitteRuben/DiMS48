130624fb1d4ec6cb5c511c25f2ed34f357606a23
Robin Staelens <robin.staelens@student.howest.be>
2018-11-22 10:25:58 +0100

    [BACKEND] Code formatting on initial in DiMS48Router

--- server/routes/tests/DiMS48Router.js
+++ server/routes/tests/DiMS48Router.js
@@ -16,15 +16,23 @@ function initial(res) {
     .then(data => res.json(data))
     .catch(err => {
       res.status(500);
-      res.json(jsonErrorMessageGenerator.generateGoogleJsonError(errorMessages.global, errorMessages.internalServerErrorReason, errorMessages.phases.couldNotGetInitial, 500));
+      res.json(
+        jsonErrorMessageGenerator.generateGoogleJsonError(
+          errorMessages.global, 
+          errorMessages.internalServerErrorReason, 
+          errorMessages.phases.couldNotGetInitial, 
+          500)
+      );
     });
 }
 
 function part2(res) {
-  getBeginObject('part2').then(data => res.json(data)).catch(err => {
-    res.status(500);
-    res.send("Could not get data for DiMS48 Part 2");
-  });
+  getBeginObject('part2')
+    .then(data => res.json(data))
+    .catch(err => {
+      res.status(500);
+      res.send("Could not get data for DiMS48 Part 2");
+    });
 }
 
 function getResults(res) {
