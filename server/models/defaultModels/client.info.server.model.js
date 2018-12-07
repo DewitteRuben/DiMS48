d0f34f66fca7b7cd180c0e596f10aca72e693760
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-12-07 15:38:15 +0100

    made notes not required

--- server/models/defaultModels/client.info.server.model.js
+++ server/models/defaultModels/client.info.server.model.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const DiMS48Database = require('../../util/databaseGetter').DiMS48Database;
+const DiMS48Database = require("../../util/databaseGetter").DiMS48Database;
 
-
-const ClientInfoSchema = new Schema({
-    age: {type: Number, required: true},
-    schooledTill: {type: Number, required: true},
-    schooledFor: {type: Number, required: true},
-    gender: {type: String, required: true, enum: ['m', 'v', 'a']},
-    notes: {type: String, required: true}
-}, {_id: false});
+const ClientInfoSchema = new Schema(
+  {
+    age: { type: Number, required: true },
+    schooledTill: { type: Number, required: true },
+    schooledFor: { type: Number, required: true },
+    gender: { type: String, required: true, enum: ["m", "v", "a"] },
+    notes: { type: String, required: false }
+  },
+  { _id: false }
+);
 
 module.exports = {
-  model: DiMS48Database.model('ClientInfo', ClientInfoSchema),
+  model: DiMS48Database.model("ClientInfo", ClientInfoSchema),
   schema: ClientInfoSchema
 };
