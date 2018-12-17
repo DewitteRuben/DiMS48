7b063f2af8914dc5bd842161984221a1d74bee1e
Robin Staelens <robin.staelens@student.howest.be>
2018-12-17 15:09:47 +0100

    Added tests for instruction seeder

--- server/test/seeders/image.seeder.spec.js
+++ server/test/seeders/image.seeder.spec.js
@@ -1,6 +1,3 @@
-const sinon = require('sinon');
-const should = require('chai').should();
-const expect = require('chai').expect;
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
