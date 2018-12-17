686e5a0b857905465ae2e1475b93753b6fd53152
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-12-17 13:44:07 +0100

    fixed formatting

--- client/src/views/Dims48View.vue
+++ client/src/views/Dims48View.vue
@@ -14,9 +14,9 @@
       :decline="true"
     />
   </v-container>
-</template>  
-  
-  <script>
+</template>
+
+<script>
 import Dims48aTest from "@/components/Dims48aTest.vue";
 import Dims48bTest from "@/components/Dims48bTest.vue";
 import ConfirmationDialog from "@/components/ConfirmationDialog.vue";
