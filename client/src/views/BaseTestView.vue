3e22e79c6093b5fab9e83c90479d7355129876fe
Unknown <jonas.de.ridder@student.howest.be>
2018-12-18 10:04:54 +0100

    BaseTestView can be set to fullscreen

--- client/src/views/BaseTestView.vue
+++ client/src/views/BaseTestView.vue
@@ -1,10 +1,26 @@
 <template>
-    <router-view></router-view>
+    <fullscreen ref="fullscreen" :fullscreen.sync="fullscreen">
+      <router-view></router-view>
+      <v-btn id="setFullscreen" type="button" @click="toggle" >Fullscreen</v-btn>
+    </fullscreen>
 </template>
 
 <script>
-export default {};
+import Fullscreen from 'vue-fullscreen/src/component.vue';
+export default {
+  components: {Fullscreen},
+  data(){
+    return {
+      fullscreen: false
+    }
+  },
+  methods:{
+    toggle(){
+      this.$refs['fullscreen'].toggle();
+    }
+  }
+};
 </script>
 
-<style>
+<style scoped>
 </style>
