f0c107db1aca509af04d4d9bc4a8140c7c414ab2
Unknown <jonas.de.ridder@student.howest.be>
2018-12-18 14:41:18 +0100

    [html] accept pdf

--- client/src/components/FileUploadForm.vue
+++ client/src/components/FileUploadForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form enctype="multipart/form-data" :action="action" method="post">
-    <input type="file" name="toUpload"/>
+    <input type="file" name="toUpload" accept="application/pdf"/>
     <input type="submit" value="Upload">
   </form>
 </template>
