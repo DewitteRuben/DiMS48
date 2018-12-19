3836a8508b81ab52ab6bbfb7a0d242e5e3ca4992
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-12-19 16:13:06 +0100

    added spinner to admin panel

--- client/src/views/AdminPanelTestView.vue
+++ client/src/views/AdminPanelTestView.vue
@@ -1,16 +1,22 @@
 <template>
   <v-container>
-    <v-layout align-center justify-center fill-height>
+    <v-layout align-center justify-center fill-height v-show="dataLoaded">
       <v-flex xs12 sm8 lg6>
         <h1>Configuratie {{$route.params.name}} Test</h1>
-        <v-form class="mt-5" v-show="dataLoaded">
-          <Dims48AdminPanel v-if="$route.params.name == 'dims48'"
-            @loaded="dataLoaded=true" @updateParentWithChildData="updateValues"/>
+        <v-form class="mt-5">
+          <Dims48AdminPanel
+            v-if="$route.params.name == 'dims48'"
+            @loaded="dataLoaded=true"
+            @updateParentWithChildData="updateValues"
+          />
           <v-btn @click="submitValues">Opslaan</v-btn>
         </v-form>
-        <br/>
+        <br>
         <FileUploadForm/>
       </v-flex>
+    </v-layout>
+    <v-layout justify-center align-center fill-height v-show="!dataLoaded">
+      <v-progress-circular :size="65" color="primary" indeterminate></v-progress-circular>
     </v-layout>
   </v-container>
 </template>
@@ -22,7 +28,8 @@ import FileUploadForm from "@/components/FileUploadForm.vue";
 
 export default {
   components: {
-    Dims48AdminPanel, FileUploadForm
+    Dims48AdminPanel,
+    FileUploadForm
   },
   data: function() {
     return {
@@ -33,15 +40,20 @@ export default {
   methods: {
     submitValues: function() {
       let self = this;
-      if(this.currentConfig){
-        this.$store.dispatch(`${self.$route.params.name}Config/updateConfigValues`, self.currentConfig);
+      if (this.currentConfig) {
+        this.$store.dispatch(
+          `${self.$route.params.name}Config/updateConfigValues`,
+          self.currentConfig
+        );
         howtotestapi
-          .updateConfig(self.$route.params.name, { newConfig: self.currentConfig })
+          .updateConfig(self.$route.params.name, {
+            newConfig: self.currentConfig
+          })
           .then(data => console.log(data));
       }
     },
-    updateValues: function(newConfig){
-      this.currentConfig = newConfig
+    updateValues: function(newConfig) {
+      this.currentConfig = newConfig;
     }
   }
 };
