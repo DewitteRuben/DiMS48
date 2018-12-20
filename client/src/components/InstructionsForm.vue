08fbb42fcef2c4c8c1bfc0db4a80453be293999a
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-12-20 15:19:44 +0100

    fixed instruction form styling

--- client/src/components/InstructionsForm.vue
+++ client/src/components/InstructionsForm.vue
@@ -1,13 +1,20 @@
 <template>
-  <div :class="{ 'grey lighten-2': currentInstruction.title === 'Testleider' }">
+  <v-layout
+    class="InstructionForm-style"
+    column
+    justify-center
+    align-center
+    :class="{ 'grey lighten-2': currentInstruction.title === 'Testleider' }"
+  >
     <h1 class="display-3 mb-3">Instructies</h1>
     <h2 class="display-2 mb-4">{{ currentInstruction.title }}</h2>
-    <div class="InstructionForm-box">
-      <p class="headline InstructionForm-message">{{ currentInstruction.message }}</p>
+    <v-layout pa-3 justify-center align-center class="headline InstructionForm-message">
+      <v-flex xs8 v-html="currentInstruction.message"></v-flex>
+    </v-layout>
+    <v-layout justify-center align-center>
       <v-btn @click="submit" bottom large>{{ buttonText }}</v-btn>
-      <div></div>
-    </div>
-  </div>
+    </v-layout>
+  </v-layout>
 </template>
 
 <script>
@@ -29,13 +36,17 @@ export default {
 </script>
 
 <style>
-.InstructionForm-box {
+/* .InstructionForm-box {
   margin-left: auto;
   margin-right: auto;
   width: 70%;
-}
+} */
 
 .InstructionForm-message {
   text-align: justify;
+}
+
+.InstructionForm-style {
+  border-radius: 25px;
 }
 </style>
