84b95ac7be20f7ddfca6873760448a359dc2f3b1
Robin Staelens <robin.staelens@student.howest.be>
2018-12-21 01:28:32 +0100

    Prepared files for testing initial repositories

---
+++ server/test/data/initialDiMS48/initial.image.spec.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const initialImageRepository = require('../../../data/initialDiMS48/images/initialImage.repository');
---
+++ server/test/data/initialDiMS48/initial.options.spec.js
@@ -0,0 +1,6 @@
+const expect = require('chai').expect;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const initialOptionsRepository = require('../../../data/initialDiMS48/options/initialOptions.repository');
