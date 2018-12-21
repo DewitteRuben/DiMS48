a9110f1c724d1c308e572d690b28041e2e094e0e
Robin Staelens <robin.staelens@student.howest.be>
2018-12-21 09:56:55 +0100

    Added tests for initial tests repository

--- server/test/data/tests/initial.tests.repository.spec.js
+++ server/test/data/tests/initial.tests.repository.spec.js
@@ -4,3 +4,24 @@ const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 
 const initialTestsRepository = require('../../../data/tests/initialTests.repository');
+
+describe('initialTestsRepository', () => {
+    describe('getDatabaseModel', () => {
+        it('should return a functions', () => {
+            const databaseModel = initialTestsRepository.getDatabaseModel();
+            databaseModel.should.be.a('function');
+        });
+    });
+
+    describe('getTests', () => {
+        it('should return an array', () => {
+            const initialTests = initialTestsRepository.getTests();
+            initialTests.should.be.an('array');
+        });
+
+        it('should return an array with size larger than 0', () => {
+            const initialTests = initialTestsRepository.getTests();
+            initialTests.length.should.be.greaterThan(0);
+        });
+    });
+});
