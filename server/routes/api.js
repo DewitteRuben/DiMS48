272acdf97463a1da76395bfa65809ba6630daf6d
Unknown <jonas.de.ridder@student.howest.be>
2018-12-21 08:53:07 +0100

    final refactor

--- server/routes/api.js
+++ server/routes/api.js
@@ -31,225 +31,56 @@ router.get('/detail/:name', function (req, res) {
 });
 
 router.post('/test/:name/updateConfig', function (req, res) {
-  if(req.session.userId){
-    UserController.isAdmin(req.session.userId).then(isAdmin=>{
-      if(isAdmin){
-        const requestedTestName = req.params.name.toLowerCase();
-        const router = routerGetter.getRouter(requestedTestName);
-
-        if (router) {
-          if (routerHasFunction(router, "updateConfig")) {
-            router.updateConfig(req, res);
-          } else {
-            errorSender.sendInvalidEndpointRequested(req, res);
-          }
-
-        } else {
-          sendTestNotFound(req, res);
-        }
-      }else{
-        errorSender.sendInvalidEndpointRequested(req,res);
-      }
-    }).catch(err=>{
-      errorSender.sendInternalServerError(req,res, err);
-    })
-  }else{
-    errorSender.sendInvalidEndpointRequested(req,res);
-  }
+  adminOnlyTestSpecificAction(req,res,"updateConfig");
 });
 
 router.get('/test/:name/initial', function (req, res) {
-  const requestedTestName = req.params.name.toLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if (routerHasFunction(router, "getInitial")) {
-      router.getInitial(req, res);
-    } else {
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"getInitial");
 });
 
 router.get('/test/:name/part2', function (req, res) {
-  const requestedTestName = req.params.name.toLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getPart2")){
-      router.getPart2(req, res);
-    }else{
-      sendInvalidEndpointRequested();
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"getPart2");
 });
 
 router.get('/results/:name', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getResults")){
-      router.getResults(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"getResults");
 });
 
 router.get('/results/:name/:id', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getResult")){
-      router.getResult(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"getResult");
 });
 
 router.patch('/results/:name/:id', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "patchClientInfoOrNote")){
-      router.patchClientInfoOrNote(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound();
-  }
+  testSpecificAction(req,res,"patchClientInfoOrNote");
 });
 
 router.delete('/results/:name/:id', function(req,res){
-  if(req.session.userId){
-    UserController.isAdmin(req.session.userId).then(isAdmin=>{
-      if(isAdmin){
-        const requestedTestName = req.params.name.toLocaleLowerCase();
-        const router = routerGetter.getRouter(requestedTestName);
-
-        if(router){
-          if(routerHasFunction(router, "deleteResult")){
-            router.deleteResult(req, res);
-          }else{
-            errorSender.sendInvalidEndpointRequested(req,res);
-          }
-        }else{
-          errorSender.sendTestNotFound();
-        }
-      }else{
-        errorSender.sendInvalidEndpointRequested(req,res)
-      }
-    }).catch(err=>{
-      errorSender.sendInternalServerError(req,res,err);
-    })
-  }else{
-    errorSender.sendInvalidEndpointRequested(req,res);
-  }
+  adminOnlyTestSpecificAction(req,res,"deleteResult");
 })
 
 router.post('/results/:name/1', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "postResultPart1")){
-      router.postResultPart1(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested();
-    }
-
-  } else {
-    errorSender.sendTestNotFound();
-  }
+  testSpecificAction(req,res,"postResultPart1");
 });
 
 router.post('/results/:name/2', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "postResultPart2")){
-      router.postResultPart2(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"postResultPart2");
 });
 
 //TODO protect against DDOS!
 router.get('/results/:name/pdf/:id', (req, res) => {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getPdf")){
-      router.getPdf(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested();
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res,"getPdf");
 });
 
 router.get('/results/:name/excel/:id', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getExcel")){
-      router.getExcel(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound();
-  }
+  testSpecificAction(req,res,"getExcel");
 });
 
 router.get('/test/:name/normValuesExist', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getNormValuesExist")){
-      router.getNormValuesExist(req, res);
-    }else{
-      sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res, "getNormValuesExist");
 });
 
 router.get('/test/:name/normValues', function (req, res) {
-  const requestedTestName = req.params.name.toLocaleLowerCase();
-  const router = routerGetter.getRouter(requestedTestName);
-
-  if (router) {
-    if(routerHasFunction(router, "getNormValues")){
-      router.getNormValues(req, res);
-    }else{
-      errorSender.sendInvalidEndpointRequested(req, res);
-    }
-  } else {
-    errorSender.sendTestNotFound(req, res);
-  }
+  testSpecificAction(req,res, "getNormValues");
 });
 
 router.post('/register', function (req, res) {
@@ -321,6 +152,36 @@ router.post('/upload/:name', function (req, res) {
     res.redirect('/');
   });
 });
+
+const adminOnlyTestSpecificAction = function(req,res,functionName){
+  if(req.session.userId){
+    UserController.isAdmin(req.session.userId).then(isAdmin=>{
+      if(isAdmin){
+        testSpecificAction(req,res,functionName);
+      }else{
+        errorSender.sendInvalidEndpointRequested(req,res)
+      }
+    }).catch(err=>{
+      errorSender.sendInternalServerError(req,res,err);
+    })
+  }else{
+    errorSender.sendInvalidEndpointRequested(req,res);
+  }
+}
+
+const testSpecificAction = function(req, res, functionName){
+  const requestedTestName = req.params.name.toLocaleLowerCase();
+  const router = routerGetter.getRouter(requestedTestName);
+  if(router){
+    if(routerHasFunction(router, functionName)){
+      router[functionName](req,res);
+    }else{
+      errorSender.sendInvalidEndpointRequested(req,res);
+    }
+  }else{
+    errorSender.sendTestNotFound(req,res);
+  }
+}
 
 const routerHasFunction = function routerHasFunction(router, functionToCheck){
   return typeof router[functionToCheck] !== "undefined";
