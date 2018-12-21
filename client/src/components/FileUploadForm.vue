7e6a67d0861e80ab88e2dee1bbb33bee21d7121a
Ruben Dewitte <ruben.dewitte@student.howest.be>
2018-12-21 10:08:14 +0100

    added base uploadbutton

--- client/src/components/FileUploadForm.vue
+++ client/src/components/FileUploadForm.vue
@@ -1,15 +1,34 @@
 <template>
-  <form enctype="multipart/form-data" :action="action" method="post">
+  <!-- <form enctype="multipart/form-data" :action="action" method="post">
     <input type="file" name="toUpload" accept="application/pdf">
     <v-btn color="primary" type="submit">Upload</v-btn>
-  </form>
+  </form>-->
+  <v-form>
+    <UploadButton :fileChangedCallback="fileChanged">
+      <template slot="icon">
+        <v-icon color="white">add</v-icon>
+      </template>
+    </UploadButton>
+  </v-form>
 </template>
 
 <script>
+import UploadButton from "vuetify-upload-button";
+
 export default {
   computed: {
     action() {
       return `/api/upload/dims48`;
+    }
+  },
+  components: {
+    UploadButton
+  },
+  methods: {
+    fileChanged(file) {
+      // handle file here. File will be an object.
+      // If multiple prop is true, it will return an object array of files.
+      console.log(file);
     }
   }
 };
--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,17 @@
 import "@babel/polyfill";
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
-import App from './App.vue';
-import router from './router';
-import store from './store/store.js';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
+import App from "./App.vue";
+import router from "./router";
+import store from "./store/store.js";
 
 Vue.use(Vuetify);
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
